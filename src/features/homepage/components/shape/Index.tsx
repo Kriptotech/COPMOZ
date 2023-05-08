878af92b9cc5014194b003aa8b1ba8e5780050cc
kriptotech <kriptotech2022@gmail.com>
2023-05-08 14:56:23 +0200

    new6

--- src/features/homepage/components/shape/Index.tsx
+++ src/features/homepage/components/shape/Index.tsx
@@ -3,8 +3,12 @@ import style from "./styles.module.css";
 export function Shape() {
     return (
         <div className={style.container}>
-            <h1>Molde seu futuro no Colégio Politécnico de Moçambique</h1>
-            <a href="/sobre">SAIBA MAIS</a>
+            <h1 data-aos="fade-down">
+                Molde seu futuro no Colégio Politécnico de Moçambique
+            </h1>
+            <a href="/sobre" data-aos="fade-up">
+                SAIBA MAIS
+            </a>
         </div>
     );
 }
--- src/features/homepage/components/why_choose/Index.tsx
+++ src/features/homepage/components/why_choose/Index.tsx
@@ -3,16 +3,18 @@ import style from "./styles.module.css";
 export function WhyChooseUs() {
     return (
         <div className={style.container}>
-            <strong>PORQUE ESCOLHER-NOS</strong>
-            <h1>
-                Você terá conhecimento dos assuntos que serão especialmente
-                úteis para você ao subir na carreira
-            </h1>
-            <p>
-                Torne-se parte de nossa faculdade para desenvolver suas
-                habilidades ocultas e se tornar um profissional.
-            </p>
-            <a href="/sobre">LEAIA MAIS</a>
+            <div data-aos="fade-right">
+                <strong>PORQUE ESCOLHER-NOS</strong>
+                <h1>
+                    Você terá conhecimento dos assuntos que serão especialmente
+                    úteis para você ao subir na carreira
+                </h1>
+                <p>
+                    Torne-se parte de nossa faculdade para desenvolver suas
+                    habilidades ocultas e se tornar um profissional.
+                </p>
+                <a href="/sobre">LEAIA MAIS</a>
+            </div>
         </div>
     );
 }
